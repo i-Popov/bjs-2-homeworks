baff7c95fbdd67a5e81bcedd34c62e81c71875ff
Igor Popov <reddym183@gmail.com>
2021-12-21 22:31:31 +0300

    Task 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [];
+        this.marks.push(mark);
+    } else {
+        this.marks.push(mark);
+    }
+};
+
+Student.prototype.addMarks = function ( ...args) {
+    if (this.marks === undefined) {
+        this.marks = [];
+        args.map(elem => this.marks.push(elem));
+    } else {
+        args.map(elem => this.marks.push(elem));
+    }
+};
+
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    this.marks.map(elem => sum += elem);
+    return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
